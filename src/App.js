4d55975304bc142be4d503dbb10cebf7e2473a37
ajaygbl <ajaygbl@gmail.com>
2024-03-31 11:49:44 +0530

    using map & showing values on web page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 function App() {
-  const [resp, setData] = useState([])
+  const [resp, setData] = useState([{}])
 
   useEffect(() => {
     fetch("/argo").then(
@@ -14,7 +14,7 @@ function App() {
   )
   }, [])
   return (<div className="card">
-    {typeof resp.data === "undefined" ? (<p>loading</p>) : (<p>{resp.data.service.name}</p>) }
+    {typeof resp.data === "undefined" ? (<p>loading</p>) : (resp.data.service.metrics[0].dimensions.map((el, i) => (<p key={i}>{el}</p>))) }
     </div>)
 }
 export default App;
