2c225cc85fd4ab7e6335d4c4fa4ca485e2f07a78
ajaygbl <ajaygbl@gmail.com>
2024-03-31 16:10:03 +0530

    added slo uptime data too

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 function App() {
   const [resp, setData] = useState([{}])
+  const [slovar, setSlovar] = useState([{}])
 
   useEffect(() => {
     fetch("/argo").then(
@@ -13,11 +14,27 @@ function App() {
     }
   )
   }, [])
+
+  useEffect(() => {
+    fetch("/argo/slo").then(
+      res => res.json()).then(
+        slovar => {
+          setSlovar(slovar)
+          console.log(slovar)
+    }
+  )
+  }, [])
+
   //return (<div className="card">
   //  {typeof resp.data === "undefined" ? (<p>loading</p>) : (resp.data.service.metrics[0].dimensions.map((el, i) => (<p key={i}>{el.value}</p>))) }
   //  </div>)
   return (<div className="card">
+  {typeof resp.data === "undefined" ? (<p>loading</p>) : (<p>{resp.data.service.name}</p>) }
   {typeof resp.data === "undefined" ? (<p>loading</p>) : (resp.data.service.metrics.map((el , i) => (<p key={i}>{el.dimensions[0].value}</p>)))}
+  <div>
+  {typeof slovar.data === "undefined" ? (<p>loading</p>) : (slovar.data.endpoints[0].previous_week_uptime.__typename)} : {typeof slovar.data === "undefined" ? (<p>loading</p>) : (slovar.data.endpoints[0].previous_week_uptime.percent)}
+
+  </div>
   </div>)
 }
 export default App;
