2c4925ecb8c019684ec1f2ff135551a06db2be77
ajaygbl <ajaygbl@gmail.com>
2024-03-31 07:56:58 +0530

    score card UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [resp, setData] = useState([])
 
+  useEffect(() => {
+    fetch("/argo").then(
+      res => res.json()).then(
+        resp => {
+          setData(resp)
+          console.log(resp.data.service.name)
+    }
+  )
+  }, [])
+  return (<div>{resp.data.service.name}</div>)
+}
 export default App;
